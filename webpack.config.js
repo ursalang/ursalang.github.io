997de58d9d4d5faabea51b39f957dc76acac054a
Reuben Thomas <rrt@sc3d.org>
2023-10-22 17:32:21 +0100

    Use DarkGlass’s new dev web server instead of “webpack serve”
    Hence simplify the webpack configuration, and run “static-site” explicitly
    in CI.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,15 @@
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
-const HookShellScriptPlugin = require('hook-shell-script-webpack-plugin')
 const WatchExternalFilesPlugin = require('webpack-watch-files-plugin').default
 
 module.exports = {
     entry: './js/main.js',
     mode: 'development',
-    devServer: {
-        static: { directory: `${__dirname}/_site` }
-    },
     output: {
         path: `${__dirname}/pages`,
         filename: 'bundle.js',
     },
     plugins: [
         new NodePolyfillPlugin(),
-        new HookShellScriptPlugin({
-            afterEmit: [{
-                command: `${__dirname}/cgi-bin/static-site`,
-                args: ['--verbose', '--force', './cgi-bin/web.pl', './_site'],
-            }],
-        }),
         new WatchExternalFilesPlugin({ files: ['./pages/**/*', './cgi-bin/**/*'] }),
     ],
     resolve: {
