404d724ea440f470f0df86d6039eed4f0fe6578f
Reuben Thomas <rrt@sc3d.org>
2023-12-28 22:06:00 +0000

    Render release notes to HTML from Markdown (fix #26)

--- js/readme.js
+++ js/readme.js
@@ -1,3 +1,5 @@
+import { marked } from "marked"
+import DOMPurify from "dompurify"
 import { Octokit } from 'octokit'
 import { debouncedUpdate, evaluate, evaluateUrsa, respondToVisibility } from './main.js'
 $(async function () {
@@ -24,5 +26,5 @@ $(async function () {
     $('#ursa-latest-release-version').text(latestRelease.data.tag_name)
     const date = new Date(Date.parse(latestRelease.data.created_at))
     $('#ursa-latest-release-date').text(date.toLocaleDateString(undefined, { day: 'numeric', month: 'long', year: 'numeric' }))
-    $('#ursa-latest-release-notes').text(latestRelease.data.body)
+    $('#ursa-latest-release-notes').html(DOMPurify.sanitize(marked.parse(latestRelease.data.body)))
 })
